a61877ee91d2717775cb2df28e5d48d9f54aa1c3
dianneyutiamco <cdyutiamco@gmail.com>
2019-10-25 18:21:12 +0800

    List

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,1 +1,6 @@
 // TODO : Component for List Components in the Menu
+// Look into https://medium.com/@martin_hotell/react-children-composition-patterns-with-typescript-56dfc8923c64
+// Consumer will use <List>...</List> this way:
+{/* <List>
+  Children
+</List> */}
